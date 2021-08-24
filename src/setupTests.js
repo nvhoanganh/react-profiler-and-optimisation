cae7b39cf57481ff4be318067e757bc91ee99e2b
Anthony Nguyen <nvhoanganh1909@gmail.com>
2021-08-24 14:26:25 +1000

    add enzyme tests

--- src/setupTests.js
+++ src/setupTests.js
@@ -4,4 +4,7 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
 
+import { configure } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
+configure({ adapter: new Adapter() });
